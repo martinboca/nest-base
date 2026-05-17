15cbfc2ff5e29c4fcdde8f3dc9177986233eae07
Martín Bocanegra <ebocanegra@fi.uba.ar>
2026-05-17 14:12:00 -0300

    fix: base entity

--- src/entity-modules/base.schema.ts
+++ src/entity-modules/base.schema.ts
@@ -1,12 +1,19 @@
 import { BaseEntity as BE, defineEntity, p } from '@mikro-orm/core';
+import { randomUUID } from 'crypto';
 
 export const BaseSchema = defineEntity({
   name: 'BaseEntity',
   extends: BE,
   abstract: true,
   properties: {
-    id: p.uuid().primary(),
+    id: p
+      .uuid()
+      .primary()
+      .onCreate(() => randomUUID()),
     createdAt: p.datetime().onCreate(() => new Date()),
-    updatedAt: p.datetime().onUpdate(() => new Date()),
+    updatedAt: p
+      .datetime()
+      .onCreate(() => new Date())
+      .onUpdate(() => new Date()),
   },
 });
